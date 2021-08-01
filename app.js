e812310f38d6a6e3c56e98613ad17b66d974f9c5
Vladislaw1 <48916709+Vladislaw1@users.noreply.github.com>
2021-08-01 20:28:16 +0300

    qwe

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express")
 const cors = require("cors")
+const mongoose = require("mongoose")
+
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const {DB_HOST} = process.env
 
 const app = express()
 
 app.use(cors())
 
-app.use("/", () => {
-    return (`<p>Hello World</p>`)
+app.use("/", (req,res,next) => {
+    res.end(`<p>Hello World</p>`)
 })
 
-app.listen(4001, () => {
-    console.log("start server")
+mongoose.connect(DB_HOST,{
+    useFindAndModify: false,
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(()=>{
+    app.listen(4001, () => {
+        console.log("start server")
+    })
 })
+
